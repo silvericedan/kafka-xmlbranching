95e9492a354e557adfa217b4bba2aecbccf18bae
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-05 09:42:33 -0300

    initial commit

---
+++ src/main/java/com/example/xmlbranching/XmlBranchingApplication.java
@@ -0,0 +1,13 @@
+package com.example.xmlbranching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlBranchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlBranchingApplication.class, args);
+	}
+
+}
